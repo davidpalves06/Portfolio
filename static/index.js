98925758963fe4856d4269caaa9304b9ed058cc8
davidpalves06 <davidpalves06@gmail.com>
2025-01-22 23:39:56 +0000

    Work experience added

--- static/index.js
+++ static/index.js
@@ -1,6 +1,10 @@
 const navBar = document.getElementById("navBar")
 const aboutNav = document.getElementById("aboutNavigation")
 const aboutSection = document.getElementById("about")
+const experienceNav = document.getElementById("experienceNavigation")
+const experienceSection = document.getElementById("experience")
+const educationNav = document.getElementById("educationNavigation")
+const educationSection = document.getElementById("education")
 const projectNav = document.getElementById("projectsNavigation")
 const projectSection = document.getElementById("projects")
 const skillsNav = document.getElementById("skillsNavigation")
@@ -12,6 +16,8 @@ let mouseY = 0;
 
 function resetStyles() {
     aboutNav.setAttribute("data-onscreen", "false")
+    experienceNav.setAttribute("data-onscreen", "false")
+    educationNav.setAttribute("data-onscreen", "false")
     projectNav.setAttribute("data-onscreen", "false")
     skillsNav.setAttribute("data-onscreen", "false")
     contactNav.setAttribute("data-onscreen", "false")
@@ -29,6 +35,8 @@ function handleScroll() {
     if (!ticking) {
         window.requestAnimationFrame(function () {
             let aboutOffsetTop = aboutSection.offsetTop + (aboutSection.offsetHeight / 2)
+            let experienceOffsetTop = experienceSection.offsetTop + (experienceSection.offsetHeight / 2)
+            let educationOffsetTop = educationSection.offsetTop + (educationSection.offsetHeight / 2)
             let projectsOffsetTop = projectSection.offsetTop + (projectSection.offsetHeight / 2)
             let skillsOffsetTop = skillsSection.offsetTop + (skillsSection.offsetHeight / 2)
             let contactOffsetTop = contactSection.offsetTop + (contactSection.offsetHeight / 2)
@@ -36,6 +44,10 @@ function handleScroll() {
             resetStyles()
             if (currentPos >= 0 && currentPos < aboutOffsetTop) {
                 aboutNav.setAttribute("data-onscreen", "true")
+            } else if (currentPos < experienceOffsetTop) {
+                experienceNav.setAttribute("data-onscreen", "true")
+            } else if (currentPos < educationOffsetTop) {
+                educationNav.setAttribute("data-onscreen", "true")
             } else if (currentPos < projectsOffsetTop) {
                 projectNav.setAttribute("data-onscreen", "true")
             } else if (currentPos < skillsOffsetTop) {
@@ -51,7 +63,6 @@ function handleScroll() {
             }
 
             previousPosition = currentPos
-            console.log("Scroll event handled");
             ticking = false;
         });
         ticking = true;
